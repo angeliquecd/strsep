0099f7936b295dd2eb8e72223de23f7cc76746ec
Angelique <acharles-davis00@stuy.edu>
2019-11-26 17:34:34 -0500

    got it to work with hardcoded input

--- main.c
+++ main.c
@@ -2,24 +2,45 @@
 #include<unistd.h>
 #include<string.h>
 #include <stdio.h>
-
+#include<errno.h>
 char ** parse_args(char *line){
-  char * curr= line;
+  char * curr=line;
   char * token;
-  char ** toreturn;
+  char ** toreturn = malloc(7 *sizeof(char*));
   int i=0;
-  while (curr && i<1){
+  while (curr){
+  *(toreturn+i) = curr;
   token = strsep(&curr," ");
-  printf("%s",token);
-  //char *(*toreturn +i) = curr;
-  i++;
+  //printf("This: %s ",toreturn[i]);
+   i++;
 }
+*(toreturn +i) = NULL;
+//printf("%s",toreturn[0]);
 printf("\n");
 return toreturn;
 }
 int main(int argc, char *argv[]){
 //FILE r;
 //fgets(argv, 100, r);
-parse_args("Hello my name is Angelique");
+char first[100] ="ls -a -l";
+char ** args=parse_args(first);
+//printf("%s\n",args[0]);
+//printf("%s\n",args[1]);
+int i=0;
+/*while (i<4){
+  printf("This is an argument: %s\n",args[i]);
+  i++;
+}
+char * cmd[3];
+cmd[0]="ls";
+cmd[1]="-a";
+cmd[2]="-l";
+
+i=execvp("ls",cmd);
+if (i<0) printf("error with ls %d\n",errno); */
+
+execvp(args[0],args);
+if (i<0) printf("error with ls %d\n",errno);
+
   return 0;
 }
