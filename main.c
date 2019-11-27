92af21db0bcbc1cea1ece7ea0f31d65e29f3d8bf
Angelique <acharles-davis00@stuy.edu>
2019-11-26 20:21:46 -0500

    sort of works

--- main.c
+++ main.c
@@ -9,19 +9,19 @@ char ** parse_args(char *line){
   char ** toreturn = malloc(6 * sizeof(char*));
   int i=0;
   while (curr){
-  *(toreturn+i) = curr;
+toreturn[i] = curr;
   token = strsep(&curr," ");
   //printf("This: %s ",toreturn[i]);
    i++;
 }
-*(toreturn +i) = NULL;
+toreturn[i] = NULL;
 //printf("%s",toreturn[0]);
 printf("\n");
 return toreturn;
 }
 
 int main(int argc, char *argv[]){
-char *r;
+char r[50];
 fgets(r, 50, stdin);
 char first[100] ="which ls";
 printf("Input: %s",r);
@@ -41,7 +41,7 @@ cmd[3]=NULL;*/
 
 //i=execvp("ls",cmd);
 //if (i<0) printf("error with ls %d\n",errno);
-i=execvp(args[0],args);
+execvp(args[0],args);
 if (i<0) printf("error with ls %d\n",errno);
 free(args);
 
