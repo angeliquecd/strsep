5d9f13114d31d05bad74f0308a8c9cafa8f15c05
Angelique <acharles-davis00@stuy.edu>
2019-11-26 20:15:00 -0500

    working on person interface thing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 char ** parse_args(char *line){
   char * curr=line;
   char * token;
-  char ** toreturn = malloc(7 *sizeof(char*));
+  char ** toreturn = malloc(6 * sizeof(char*));
   int i=0;
   while (curr){
   *(toreturn+i) = curr;
@@ -24,23 +24,24 @@ int main(int argc, char *argv[]){
 char *r;
 fgets(r, 50, stdin);
 char first[100] ="which ls";
+printf("Input: %s",r);
 char ** args=parse_args(r);
 //printf("%s\n",args[0]);
 //printf("%s\n",args[1]);
 int i=0;
-while (i<4){
+while (i<6){
   printf("This is an argument: %s\n",args[i]);
   i++;
 }
-/*char * cmd[3];
+/*char * cmd[4];
 cmd[0]="ls";
 cmd[1]="-a";
 cmd[2]="-l";
+cmd[3]=NULL;*/
 
-i=execvp("ls",cmd);
-if (i<0) printf("error with ls %d\n",errno); */
-
-execvp(args[0],args);
+//i=execvp("ls",cmd);
+//if (i<0) printf("error with ls %d\n",errno);
+i=execvp(args[0],args);
 if (i<0) printf("error with ls %d\n",errno);
 free(args);
 
