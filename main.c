6867aec793f0310e5f92513afc5860c066152780
Angelique <acharles-davis00@stuy.edu>
2019-11-27 21:52:48 -0500

    fixed it to work in command line

--- main.c
+++ main.c
@@ -9,10 +9,12 @@ char ** parse_args(char *line){
   char ** toreturn = malloc(6 * sizeof(char*));
   int i=0;
   while (curr){
-toreturn[i] = strsep(&curr," ");
+    token = strsep(&curr," ");
+toreturn[i] = token;
   //printf("This: %s ",toreturn[i]);
    i++;
 }
+toreturn[i-1] = strsep(&(toreturn[i-1]), "\n"); //removes last escape
 toreturn[i] = NULL;
 //printf("%s",toreturn[0]);
 //printf("\n");
@@ -21,10 +23,10 @@ return toreturn;
 
 int main(int argc, char *argv[]){
 char r[50];
-//fgets(r, 50, stdin);
+fgets(r, 50, stdin);
 char first[100] ="ls -a -l";
 //printf("Input: %s %s\n",argv[1],argv[2]);
-char ** args=parse_args(first);
+char ** args=parse_args(r);
 //printf("%s\n",args[0]);
 //printf("%s\n",args[1]);
 printf("Running command.\n");
@@ -32,13 +34,13 @@ int i=0;
 /*while (i<6){
   printf("This is an argument: %s\n",args[i]);
   i++;
-}*/
-/*char * cmd[4];
+}
+char * cmd[4];
 cmd[0]="ls";
 cmd[1]="-a";
 cmd[2]="-l";
-cmd[3]=NULL;*/
-
+cmd[3]=NULL;
+*/
 //i=execvp("ls",cmd);
 //if (i<0) printf("error with ls %d\n",errno);
 execvp(args[0],args);
